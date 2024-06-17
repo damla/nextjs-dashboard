c1c927082f225ecbe9af2bdec3276b8c8c6bc49c
Damla Koksal <me@damlakoksal.com>
2024-06-17 20:39:06 +0300

    style: formatting applied

--- src/app/(dashboard)/page.tsx
+++ src/app/(dashboard)/page.tsx
@@ -1,3 +1,3 @@
 export default function Home() {
-  return <p>Dashboard</p>
+  return <p>Dashboard</p>;
 }
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,26 +1,26 @@
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import '@/styles/globals.css'
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
+import '@/styles/globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
   title: 'NextJS Dashboard',
   description: 'Dashboard application created with NextJS and schadcn/ui'
-}
+};
 
 export default function RootLayout({
   children
 }: Readonly<{
-  children: React.ReactNode
+  children: React.ReactNode;
 }>) {
   return (
-    <html lang='en' suppressHydrationWarning>
+    <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
-        <main className='flex min-h-screen flex-col items-center justify-between p-24'>
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
           {children}
         </main>
       </body>
     </html>
-  )
+  );
 }
