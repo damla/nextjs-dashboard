d8f2332a679eb95d6a4e94654baa04851155616d
Damla Koksal <me@damlakoksal.com>
2024-06-18 16:16:22 +0300

    feat: refactor to a Breadcrumb component

---
+++ src/components/common/breadcrumb.tsx
@@ -0,0 +1,34 @@
+import Link from 'next/link';
+
+import {
+  Breadcrumb as BreadcrumbSchadcn,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator
+} from '@/components/ui/breadcrumb';
+
+export const Breadcrumb = () => {
+  return (
+    <BreadcrumbSchadcn className="hidden md:flex">
+      <BreadcrumbList>
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="#">Dashboard</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbLink asChild>
+            <Link href="#">Orders</Link>
+          </BreadcrumbLink>
+        </BreadcrumbItem>
+        <BreadcrumbSeparator />
+        <BreadcrumbItem>
+          <BreadcrumbPage>Recent Orders</BreadcrumbPage>
+        </BreadcrumbItem>
+      </BreadcrumbList>
+    </BreadcrumbSchadcn>
+  );
+};
--- src/components/dashboard/layout/navbar.tsx
+++ src/components/dashboard/layout/navbar.tsx
@@ -1,15 +1,8 @@
 import Link from 'next/link';
 
+import { Breadcrumb } from '@/components/common/breadcrumb';
 import { ModeToggle } from '@/components/common/mode-toggle';
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar';
-import {
-  Breadcrumb,
-  BreadcrumbItem,
-  BreadcrumbLink,
-  BreadcrumbList,
-  BreadcrumbPage,
-  BreadcrumbSeparator
-} from '@/components/ui/breadcrumb';
 import { Button } from '@/components/ui/button';
 import {
   DropdownMenu,
@@ -77,25 +70,7 @@ export const Navbar = () => {
           </nav>
         </SheetContent>
       </Sheet>
-      <Breadcrumb className="hidden md:flex">
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="#">Dashboard</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink asChild>
-              <Link href="#">Orders</Link>
-            </BreadcrumbLink>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbPage>Recent Orders</BreadcrumbPage>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+      <Breadcrumb />
       <div className="relative ml-auto flex-1 md:grow-0">
         <ModeToggle />
       </div>
