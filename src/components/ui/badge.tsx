d8fdd2fe720e837bee3743f7199947524ae21895
Damla Koksal <me@damlakoksal.com>
2024-06-18 20:07:38 +0300

    refactor: done on payments table

--- src/components/ui/badge.tsx
+++ src/components/ui/badge.tsx
@@ -11,6 +11,7 @@ const badgeVariants = cva(
         default: 'border-transparent bg-primary text-primary-foreground shadow hover:bg-primary/80',
         secondary:
           'border-transparent bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        success: 'border-transparent bg-green-600 text-primary-foreground hover:bg-green-600/80',
         destructive:
           'border-transparent bg-destructive text-destructive-foreground shadow hover:bg-destructive/80',
         outline: 'text-foreground'
